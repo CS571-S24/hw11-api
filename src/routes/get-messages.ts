38bbb25991c481ac69bfeafe2646a726d152c13e
Cole Nelson <ctnelson1997@gmail.com>
2024-04-21 21:33:18 -0500

    api

---
+++ src/routes/get-messages.ts
@@ -0,0 +1,48 @@
+import { Express } from 'express';
+
+import { CS571Route } from "@cs571/s24-api-framework/src/interfaces/route";
+import { CS571HW11DbConnector } from '../services/hw11-db-connector';
+
+export class CS571GetMessagesRoute implements CS571Route {
+
+    public static readonly ROUTE_NAME: string = '/messages';
+
+    private readonly connector: CS571HW11DbConnector;
+    private readonly chatrooms: string[];
+
+    public constructor(chatrooms: string[], connector: CS571HW11DbConnector) {
+        this.chatrooms = chatrooms;
+        this.connector = connector;
+    }
+
+    public addRoute(app: Express): void {
+        app.get(CS571GetMessagesRoute.ROUTE_NAME, async (req, res) => {
+            const chatroom = (req.query.chatroom || "") as string;
+            const numPosts = parseInt((req.query.num || "1") as string);
+            if (!this.chatrooms.includes(chatroom)) {
+                res.status(404).send({
+                    msg: "The specified chatroom does not exist. Chatroom names are case-sensitive."
+                });
+                return;
+            }
+
+            if (isNaN(numPosts) || numPosts < 1 || numPosts > 100) {
+                res.status(400).send({
+                    msg: "Only between 1 and 100 posts may be returned."
+                });
+                return;
+            }
+
+            const messages = await (chatroom ? this.connector.getMessages(chatroom) : this.connector.getAllMessages())
+
+            res.status(200).send({
+                msg: "Successfully got the latest messages!",
+                messages: messages.slice(numPosts)
+            });
+        })
+    }
+
+    public getRouteName(): string {
+        return CS571GetMessagesRoute.ROUTE_NAME;
+    }
+}
